c9bc2b473f6a135a0bd1b19dacf93d39332ddf75
jd4518 <formless_@naver.com>
2015-10-28 11:36:13 +0900

    1. BusAPI URL 호출 스터디 2. Git 스터디

--- src/main/java/com/hybrid/CommonsLogTest.java
+++ src/main/java/com/hybrid/CommonsLogTest.java
@@ -1,5 +1,39 @@
 package com.hybrid;
 
-public class CommonsLogTest {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class CommonsLogTest {
+	static Log log = LogFactory.getLog(CommonsLogTest.class);
+	public static void main(String[] args) {
+		String url = "http://ws.bus.go.kr/api/rest/busRouteInfo/getBusRouteList?strSrch=6628&ServiceKey=AaxqTg02PVW%2BZhaIkh4fVAIiknK6EU6ZkfT1lQEHEo2PRlldpzfhjoBwE63YKQGpiY4JdZCjCktTW2yatRX%2FgA%3D%3D";
+		try{
+			URL u = new URL(url);
+			
+			InputStream in = u.openStream();
+			Reader r = new InputStreamReader(in);
+			BufferedReader reader = new BufferedReader(r);
+			
+			String line = null;
+			while((line = reader.readLine())!=null){
+				log.info(line);
+				
+			}
+			reader.close();
+			
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}  
+	
+	public static void test1(){
+		
+	}
+	
 }
