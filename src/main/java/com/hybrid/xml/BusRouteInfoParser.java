10c0f03ef647229dab37bf807fc0ea1162cd426b
jd4518 <formless_@naver.com>
2015-10-29 12:23:56 +0900

    OXM (Object XML Mapping) ==> 	-DOM Parser(수동) 	-JAXB(자동) ORM (Object Relational Mapping) ==> JDBC Programming

--- src/main/java/com/hybrid/xml/BusRouteInfoParser.java
+++ src/main/java/com/hybrid/xml/BusRouteInfoParser.java
@@ -2,10 +2,13 @@ package com.hybrid.xml;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.soap.Node;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
@@ -13,11 +16,20 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+
+import com.hybrid.model.RouteInfoItem;
 
 public class BusRouteInfoParser {
 	
@@ -25,11 +37,13 @@ public class BusRouteInfoParser {
 	DocumentBuilderFactory dFactory;
 	DocumentBuilder builder;
 	
+	XPathFactory xFactory;
+	
 	TransformerFactory tFactory;
 	public BusRouteInfoParser() throws ParserConfigurationException {
 		dFactory = DocumentBuilderFactory.newInstance();
 		builder = dFactory.newDocumentBuilder();
-		
+		xFactory = XPathFactory.newInstance();
 		tFactory = TransformerFactory.newInstance();
 	}
 	
@@ -38,9 +52,13 @@ public class BusRouteInfoParser {
 			try {
 				BusRouteInfoParser parser = new BusRouteInfoParser();
 				
-				String xml = parser.getBusRouteList("5422");
+				List<RouteInfoItem> list = parser.getBusRouteList("5422");
 				
-				System.out.println(xml);
+				for(RouteInfoItem item : list){
+					log.info(item.getBusRouteId());
+					log.info(item.getBusRouteNm());
+					log.info(item.getEdStationNm());
+				}
 				
 			} catch (ParserConfigurationException e) {
 				// TODO Auto-generated catch block
@@ -48,18 +66,47 @@ public class BusRouteInfoParser {
 			}
 			log.info("Program end...");
 	}
-	public String getBusRouteList(String strSrch){
+	public List<RouteInfoItem> getBusRouteList(String strSrch){
 		log.info("getBusRouteList = "+ strSrch);
 		
 		String url = "http://ws.bus.go.kr/api/rest/busRouteInfo/getBusRouteList?strSrch="+strSrch+"&ServiceKey=AaxqTg02PVW%2BZhaIkh4fVAIiknK6EU6ZkfT1lQEHEo2PRlldpzfhjoBwE63YKQGpiY4JdZCjCktTW2yatRX%2FgA%3D%3D";
-		
+		List<RouteInfoItem> model = new ArrayList<>();
 		String result = null;
 		try {
 			
 			//unmarshall(deserialization)
 			Document document = builder.parse(url);
+			XPath xpath =  xFactory.newXPath();
+			XPathExpression expr = xpath.compile("//itemList");
+			
+			NodeList list = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
+			
+			RouteInfoItem item  = new RouteInfoItem();
+			for(int i =0; i<list.getLength();i++){
+				Element el = (Element) list.item(i);
+				NodeList childs = el.getChildNodes();
+				for(int j =0 ; j<childs.getLength();j++){
+					if(childs.item(j).getNodeType() == Node.ELEMENT_NODE){
+						
+						if(childs.item(j).getNodeName().equals("busRouteId")){
+							item.setBusRouteId(childs.item(j).getTextContent());
+						}
+						if(childs.item(j).getNodeName().equals("busRouteNm")){
+							item.setBusRouteNm(childs.item(j).getTextContent());
+						}
+						if(childs.item(j).getNodeName().equals("edStationNm")){
+							item.setEdStationNm(childs.item(j).getTextContent());
+						}
+					}
+				}
+				
+				log.info("Element Name = " + el.getNodeName());
+			}
+			model.add(item);
+			
 			
 			//Marshall (Serialization)
+			tFactory.setAttribute("indent-number", 4);
 			Transformer transformer = tFactory.newTransformer();
 			transformer.setOutputProperty(OutputKeys.INDENT,"yes");
 			
@@ -84,8 +131,11 @@ public class BusRouteInfoParser {
 		} catch (TransformerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (XPathExpressionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
-		return result;
+		return model;
 	}
 }
